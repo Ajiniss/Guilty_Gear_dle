f788f423dfa5ab2f0b27dd7f0c496316213df9de
Ajiniss <baptistekenobi@gmail.com>
2026-01-01 13:40:54 +0100

    squelette du projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { dayKeyParis } from './lib/day'
+import { dayKeyLoad } from './lib/day'
 
 export default function App() {
   return (
     <main style={{ padding: 24 }}>
-      <h1>Guilty Gear Dle</h1>
-      <p>Day key: {dayKeyParis()}</p>
+      <h1>Guilty Geardle</h1>
+      <p>Day key: {dayKeyLoad()}</p>
     </main>
   )
 }
+
 
